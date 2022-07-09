e0f38b456f83e1df99e8cee5ba3b4913ea32cc3b
vijaykumar <vijay1111mane@gmail.com>
2022-07-09 14:41:06 +0530

    commit;

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <h1>{{count}}</h1>
+  <button v-on:click="count=count+1"> + </button>
+
+  <button v-on:click="count=count-1"> - </button>
+</div>
+</template>
+
+<script>
+export default {
+    name:"Watcher-Home",
+    data(){
+        return{
+            count:0
+        }
+    },
+    watch:{
+        count(val, prev){
+            if(val>5 && val>prev){
+                this.count=0
+                // alert("stop counting")
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
